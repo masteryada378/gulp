541438cede70d5833037249ebb0f34e838821105
Yura <masteryada378@gmail.com>
2021-06-08 08:57:41 +0300

    gotovo

--- build/js/script.js
+++ build/js/script.js
@@ -33,6 +33,41 @@ function initModule() {
   }
 
   menu();
+  _yourEyes.menuOpen = true;
+
+  function menuOpen() {
+    var fragment = document.createDocumentFragment();
+    var headerElement = document.getElementById('header');
+    var gamburgElement = headerElement.querySelector('.gamburg');
+    var languige = headerElement.querySelector('.header__top-lang').cloneNode(true);
+    fragment.appendChild(languige);
+    var menu = headerElement.querySelector('.header__menu-box').cloneNode(true);
+    fragment.appendChild(menu);
+    var buttonOrd = headerElement.querySelector('.header__order').cloneNode(true);
+    fragment.appendChild(buttonOrd);
+    var headerPhoneBox = headerElement.querySelector('.header__phone-box').cloneNode(true);
+    fragment.appendChild(headerPhoneBox);
+    var headerTime = headerElement.querySelector('.header__time').cloneNode(true);
+    fragment.appendChild(headerTime);
+    var headerTopSocial = headerElement.querySelector('.header__top-social').cloneNode(true);
+    fragment.appendChild(headerTopSocial);
+    var krestik = headerElement.querySelector('#gamburg').cloneNode(true);
+    krestik.querySelector('.gamburg').classList.add('open');
+    fragment.appendChild(krestik);
+    var menuBox = headerElement.querySelector('.mobil-menu__box');
+    menuBox.appendChild(fragment);
+    _yourEyes.menuOpen = true;
+    menuBox.classList.toggle('open');
+    krestik.addEventListener('click', function () {
+      krestik.querySelector('.gamburg').classList.remove('open');
+      menuBox.classList.remove('open');
+      menuBox.innerHTML = '';
+    });
+  }
+
+  if (!!document.getElementById('gamburg')) {
+    document.getElementById('gamburg').addEventListener('click', menuOpen);
+  }
 }
 
 document.addEventListener('DOMContentLoaded', initModule);
--- source/js/modules/_mi_script.js
+++ source/js/modules/_mi_script.js
@@ -32,6 +32,51 @@ function initModule() {
         menuElement.addEventListener('mouseout', leveHandlerMenu);
     }
     menu();
+    _yourEyes.menuOpen = true;
+    
+    function menuOpen() {
+
+
+        
+        var fragment = document.createDocumentFragment();
+        let headerElement = document.getElementById('header');
+        let gamburgElement = headerElement.querySelector('.gamburg');
+        let languige = headerElement.querySelector('.header__top-lang').cloneNode(true);
+        fragment.appendChild(languige);
+        let menu = headerElement.querySelector('.header__menu-box').cloneNode(true);
+        fragment.appendChild(menu);
+        let buttonOrd = headerElement.querySelector('.header__order').cloneNode(true);
+        fragment.appendChild(buttonOrd);
+        let headerPhoneBox = headerElement.querySelector('.header__phone-box').cloneNode(true);
+        fragment.appendChild(headerPhoneBox);
+        let headerTime = headerElement.querySelector('.header__time').cloneNode(true);
+        fragment.appendChild(headerTime);
+        let headerTopSocial = headerElement.querySelector('.header__top-social').cloneNode(true);
+        fragment.appendChild(headerTopSocial);
+        let krestik = headerElement.querySelector('#gamburg').cloneNode(true);
+        krestik.querySelector('.gamburg').classList.add('open');
+        
+        fragment.appendChild(krestik);
+
+        let menuBox = headerElement.querySelector('.mobil-menu__box');
+
+        menuBox.appendChild(fragment);
+                
+        _yourEyes.menuOpen = true;
+        
+        menuBox.classList.toggle('open');
+
+        krestik.addEventListener('click', function(){
+            krestik.querySelector('.gamburg').classList.remove('open');
+            menuBox.classList.remove('open');
+            menuBox.innerHTML = '';
+        });
+    }
+    
+
+    if(!!document.getElementById('gamburg')){
+        document.getElementById('gamburg').addEventListener('click', menuOpen)
+    }
 
 }
 document.addEventListener('DOMContentLoaded', initModule);
