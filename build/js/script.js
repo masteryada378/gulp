60f79bcbd4fca04f521ee1e1a80128e572dc2289
Yura <masteryada378@gmail.com>
2021-06-09 11:33:54 +0300

    forma html

--- build/js/script.js
+++ build/js/script.js
@@ -6,30 +6,27 @@ function initModule() {
   console.log('hello ZIR');
 
   function menu() {
-    var menuElement = document.getElementById('headerMenu');
+    var menuElement = document.querySelector('.mobil-menu__box'); // let menuElement = document.getElementById('headerMenu');
 
     function handlerMenu(event) {
       if (event.target.nodeName === 'A' && !!event.target.nextElementSibling && event.target.nextElementSibling.nodeName === 'UL') {
-        if (!!_yourEyes.lastAHover) {
+        event.preventDefault();
+
+        if (!!_yourEyes.lastAHover && _yourEyes.lastAHover != event.target) {
+          console.log('1');
+
           _yourEyes.lastAHover.nextElementSibling.classList.remove('menu-hover');
         }
 
         _yourEyes.lastAHover = event.target;
 
-        _yourEyes.lastAHover.nextElementSibling.classList.add('menu-hover');
+        _yourEyes.lastAHover.nextElementSibling.classList.toggle('menu-hover');
 
-        console.dir(_yourEyes.lastAHover);
+        console.log('2');
       }
     }
 
-    function leveHandlerMenu(event) {
-      // console.log('leveHandlerMenu', event.target);
-      if (!!_yourEyes.lastAHover) {// _yourEyes.lastAHover.nextElementSibling.classList.remove('menu-hover');
-      }
-    }
-
-    menuElement.addEventListener('mouseover', handlerMenu);
-    menuElement.addEventListener('mouseout', leveHandlerMenu);
+    menuElement.addEventListener('click', handlerMenu);
   }
 
   menu();
@@ -40,23 +37,24 @@ function initModule() {
     var headerElement = document.getElementById('header');
     var gamburgElement = headerElement.querySelector('.gamburg');
     var languige = headerElement.querySelector('.header__top-lang').cloneNode(true);
-    fragment.appendChild(languige);
     var menu = headerElement.querySelector('.header__menu-box').cloneNode(true);
-    fragment.appendChild(menu);
     var buttonOrd = headerElement.querySelector('.header__order').cloneNode(true);
-    fragment.appendChild(buttonOrd);
     var headerPhoneBox = headerElement.querySelector('.header__phone-box').cloneNode(true);
-    fragment.appendChild(headerPhoneBox);
     var headerTime = headerElement.querySelector('.header__time').cloneNode(true);
-    fragment.appendChild(headerTime);
     var headerTopSocial = headerElement.querySelector('.header__top-social').cloneNode(true);
-    fragment.appendChild(headerTopSocial);
     var krestik = headerElement.querySelector('#gamburg').cloneNode(true);
+    fragment.appendChild(languige);
+    fragment.appendChild(menu);
+    fragment.appendChild(buttonOrd);
+    fragment.appendChild(headerPhoneBox);
+    fragment.appendChild(headerTime);
+    fragment.appendChild(headerTopSocial);
     krestik.querySelector('.gamburg').classList.add('open');
     fragment.appendChild(krestik);
     var menuBox = headerElement.querySelector('.mobil-menu__box');
     menuBox.appendChild(fragment);
     _yourEyes.menuOpen = true;
+    gamburgElement.classList.toggle('open');
     menuBox.classList.toggle('open');
     krestik.addEventListener('click', function () {
       krestik.querySelector('.gamburg').classList.remove('open');
@@ -67,6 +65,116 @@ function initModule() {
 
   if (!!document.getElementById('gamburg')) {
     document.getElementById('gamburg').addEventListener('click', menuOpen);
+  }
+
+  function initMask(element) {
+    var phone = element;
+
+    function setCursorPosition(pos, elem) {
+      elem.focus();
+      if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);else if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select();
+      }
+    }
+
+    function mask(event) {
+      var matrix = this.defaultValue,
+          i = 0,
+          def = matrix.replace(/\D/g, ""),
+          val = this.value.replace(/\D/g, "");
+      def.length >= val.length && (val = def);
+      matrix = matrix.replace(/[_\d]/g, function (a) {
+        return val.charAt(i++) || "_";
+      });
+      this.value = matrix;
+      i = matrix.lastIndexOf(val.substr(-1));
+      i < matrix.length && matrix != this.defaultValue ? i++ : i = matrix.indexOf("_");
+      setCursorPosition(i, this);
+    }
+
+    phone.addEventListener("input", mask, false);
+  }
+
+  var allMaskPhone = document.querySelectorAll('.inputMaska');
+
+  for (var i = 0; i < allMaskPhone.length; i++) {
+    initMask(allMaskPhone[i]);
+  }
+
+  function initFormFront(element) {
+    var dozTru = true;
+    var forma = element;
+    console.log(forma);
+
+    if (!!forma) {
+      var inputName = forma.querySelector('.inputName');
+      var inputPhone = forma.querySelector('.inputMaska');
+      var inputText = forma.querySelector('.inputText');
+      var inputButton = forma.querySelector('.buttonS');
+      console.log(inputName, inputPhone, inputText);
+      inputButton.addEventListener('click', function (eventS) {
+        console.log(eventS);
+
+        if (!!inputPhone.value.match(/^([+]?[0-9\s-\(\)]{3,25})*$/i)) {
+          inputPhone.classList.remove('invalid');
+        } else {
+          inputPhone.classList.add('invalid');
+        }
+
+        if (inputName.value.length < 4) {
+          inputName.classList.add('invalid');
+        } else {
+          inputName.classList.remove('invalid');
+        }
+
+        if (inputText.value.length < 4) {
+          inputText.classList.add('invalid');
+        } else {
+          inputText.classList.remove('invalid');
+        }
+
+        if (dozTru) {// eventS.preventDefault();
+        }
+      });
+    }
+  }
+
+  var initFormFrontElement = document.querySelector('.form-order__box');
+  initFormFront(initFormFrontElement);
+
+  function initFormSlider(element) {
+    var dozTru = true;
+    var forma = element;
+    console.log(forma);
+
+    if (!!forma) {
+      var inputName = forma.querySelector('.inputName');
+      var inputPhone = forma.querySelector('.inputMaska');
+      var inputText = forma.querySelector('.inputText');
+      var inputButton = forma.querySelector('.buttonS');
+      console.log(inputName, inputPhone, inputText);
+      inputButton.addEventListener('click', function (eventS) {
+        if (!!inputPhone.value.match(/^([+]?[0-9\s-\(\)]{3,25})*$/i)) {
+          console.log('if');
+          inputPhone.classList.remove('invalid');
+          eventS.preventDefault();
+        } else {
+          eventS.preventDefault();
+          console.log('else');
+          inputPhone.classList.add('invalid');
+        }
+      });
+    }
+  }
+
+  var elementsForm = document.querySelectorAll('.formSlider');
+
+  for (var i = 0; i < elementsForm.length; i++) {
+    initFormSlider(elementsForm[i]);
   }
 }
 
--- source/js/_module.js
+++ source/js/_module.js
@@ -1,35 +1,25 @@
 var _yourEyes = {};
-
 function initModule() {
     console.log('hello ZIR');
-
     function menu() {
-        let menuElement = document.getElementById('headerMenu');
-
+        let menuElement = document.querySelector('.mobil-menu__box');
+        // let menuElement = document.getElementById('headerMenu');
         function handlerMenu(event) {
             if(event.target.nodeName === 'A' 
                 &&  !!event.target.nextElementSibling 
                 &&  event.target.nextElementSibling.nodeName === 'UL') {
-                    if(!!_yourEyes.lastAHover){
+                    event.preventDefault();
+                    
+                    if(!!_yourEyes.lastAHover && (_yourEyes.lastAHover != event.target) ){
+                        console.log('1')
                         _yourEyes.lastAHover.nextElementSibling.classList.remove('menu-hover');
                     }
-
                     _yourEyes.lastAHover = event.target;
-
-                    _yourEyes.lastAHover.nextElementSibling.classList.add('menu-hover');
-                
-                console.dir(_yourEyes.lastAHover);
+                    _yourEyes.lastAHover.nextElementSibling.classList.toggle('menu-hover');
+                    console.log('2')
             }
         }
-
-        function leveHandlerMenu(event) {
-            // console.log('leveHandlerMenu', event.target);
-            if(!!_yourEyes.lastAHover){
-                // _yourEyes.lastAHover.nextElementSibling.classList.remove('menu-hover');
-            }
-        }
-        menuElement.addEventListener('mouseover', handlerMenu);
-        menuElement.addEventListener('mouseout', leveHandlerMenu);
+        menuElement.addEventListener('click', handlerMenu);
     }
     menu();
     _yourEyes.menuOpen = true;
@@ -38,32 +28,28 @@ function initModule() {
 
 
         
-        var fragment = document.createDocumentFragment();
+        let fragment = document.createDocumentFragment();
         let headerElement = document.getElementById('header');
         let gamburgElement = headerElement.querySelector('.gamburg');
         let languige = headerElement.querySelector('.header__top-lang').cloneNode(true);
-        fragment.appendChild(languige);
         let menu = headerElement.querySelector('.header__menu-box').cloneNode(true);
-        fragment.appendChild(menu);
         let buttonOrd = headerElement.querySelector('.header__order').cloneNode(true);
-        fragment.appendChild(buttonOrd);
         let headerPhoneBox = headerElement.querySelector('.header__phone-box').cloneNode(true);
-        fragment.appendChild(headerPhoneBox);
         let headerTime = headerElement.querySelector('.header__time').cloneNode(true);
-        fragment.appendChild(headerTime);
         let headerTopSocial = headerElement.querySelector('.header__top-social').cloneNode(true);
-        fragment.appendChild(headerTopSocial);
         let krestik = headerElement.querySelector('#gamburg').cloneNode(true);
+        fragment.appendChild(languige);
+        fragment.appendChild(menu);
+        fragment.appendChild(buttonOrd);
+        fragment.appendChild(headerPhoneBox);
+        fragment.appendChild(headerTime);
+        fragment.appendChild(headerTopSocial);
         krestik.querySelector('.gamburg').classList.add('open');
-        
         fragment.appendChild(krestik);
-
         let menuBox = headerElement.querySelector('.mobil-menu__box');
-
         menuBox.appendChild(fragment);
-                
         _yourEyes.menuOpen = true;
-        
+        gamburgElement.classList.toggle('open');
         menuBox.classList.toggle('open');
 
         krestik.addEventListener('click', function(){
@@ -77,6 +63,124 @@ function initModule() {
     if(!!document.getElementById('gamburg')){
         document.getElementById('gamburg').addEventListener('click', menuOpen)
     }
+
+    function initMask(element) {
+    let phone = element;
+    
+    function setCursorPosition(pos, elem) {
+        elem.focus();
+        if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);else if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select();
+        }
+    }
+    
+    function mask(event) {
+        var matrix = this.defaultValue,
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, "");
+        def.length >= val.length && (val = def);
+        matrix = matrix.replace(/[_\d]/g, function (a) {
+        return val.charAt(i++) || "_";
+        });
+        this.value = matrix;
+        i = matrix.lastIndexOf(val.substr(-1));
+        i < matrix.length && matrix != this.defaultValue ? i++ : i = matrix.indexOf("_");
+        setCursorPosition(i, this);
+    }
+    
+    phone.addEventListener("input", mask, false);
+    }    
+    let allMaskPhone = document.querySelectorAll('.inputMaska');
+    for (var i = 0; i < allMaskPhone.length; i++) {
+        initMask(allMaskPhone[i]);
+    }
+    
+    function initFormFront(element) {
+        let dozTru = true;
+        let forma = element;
+        console.log(forma);
+        
+        if(!!forma){
+            let inputName = forma.querySelector('.inputName');
+            let inputPhone = forma.querySelector('.inputMaska');
+            let inputText = forma.querySelector('.inputText');
+            let inputButton = forma.querySelector('.buttonS');
+            console.log(inputName,inputPhone,inputText);
+
+            
+
+
+            inputButton.addEventListener('click', function(eventS){
+                console.log(eventS);
+
+                if(!!inputPhone.value.match(/^([+]?[0-9\s-\(\)]{3,25})*$/i)){
+                    inputPhone.classList.remove('invalid');
+                } else {
+                    inputPhone.classList.add('invalid');
+                }
+
+                if(inputName.value.length < 4){
+                    inputName.classList.add('invalid');
+                } else {
+                    inputName.classList.remove('invalid');
+                }
+                if(inputText.value.length < 4){
+                    inputText.classList.add('invalid');
+                } else {
+                    inputText.classList.remove('invalid');
+                }
+                
+                if(dozTru){
+                    // eventS.preventDefault();
+                }
+            })
+        }
+        
+        
+
+    }
+    let initFormFrontElement = document.querySelector('.form-order__box');
+    initFormFront(initFormFrontElement);
+
+
+    function initFormSlider(element) {
+        let dozTru = true;
+        let forma = element;
+        console.log(forma);
+        
+        if(!!forma){
+            let inputName = forma.querySelector('.inputName');
+            let inputPhone = forma.querySelector('.inputMaska');
+            let inputText = forma.querySelector('.inputText');
+            let inputButton = forma.querySelector('.buttonS');
+            console.log(inputName,inputPhone,inputText);
+
+            
+
+
+            inputButton.addEventListener('click', function(eventS){
+                if(!!inputPhone.value.match(/^([+]?[0-9\s-\(\)]{3,25})*$/i)){
+                    console.log('if');
+                    inputPhone.classList.remove('invalid');
+                    eventS.preventDefault();
+                } else {
+                    eventS.preventDefault();
+                    console.log('else');
+                    inputPhone.classList.add('invalid');
+                }
+            })
+        }
+    }
+    let elementsForm = document.querySelectorAll('.formSlider')
+    for (var i = 0; i < elementsForm.length; i++) {
+        initFormSlider(elementsForm[i]);
+    }
+
 
 }
 document.addEventListener('DOMContentLoaded', initModule);
